d8fe5c506a73eec401dc6c081cc16c13cb99e2c6
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-11-03 19:53:22 +0300

    Created cr functionality

--- backend/src/controllers/filesControler.ts
+++ backend/src/controllers/filesControler.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import fs from "fs"
-import fsPromises from "fs/promises"
+import fs from "fs";
+import fsPromises from "fs/promises";
 import path from "path";
 export const getElemsInFolder = (req: Request, res: Response) => {
   const folderPath = req.query.path as string;
@@ -33,20 +33,64 @@ export const getFileContent = (req: Request, res: Response) => {
 };
 export const saveFileChanges = async (req: Request, res: Response) => {
   try {
-      const {path: filePath, content} = req.body;
-  if (!filePath) {
-    return res.status(400).json({ error: "No file path" });
+    const { path: filePath, content } = req.body;
+    if (!filePath) {
+      return res.status(400).json({ error: "No file path" });
+    }
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).json({ error: "No such file" });
+    }
+    if (!fs.statSync(filePath).isFile()) {
+      return res.status(400).json({ error: "Path is not a file" });
+    }
+    await fsPromises.writeFile(filePath, content ?? "", "utf-8");
+    res.json({ success: true });
+  } catch (err) {
+    console.error("Ошибка при сохранении файла", err);
+    res.status(500).json({ error: "Не удалось сохранить файл" });
   }
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).json({ error: "No such file" });
+};
+export const createFilder = async (req: Request, res: Response) => {
+  try {
+    const { path: parentPath, name, isFolder } = req.body;
+    if (!parentPath || !name) {
+      return res
+        .status(400)
+        .json({ error: "Отсутствует имя или родительская папка" });
+    }
+    const fullPath = path.join(parentPath, name);
+    if (fs.existsSync(fullPath)) {
+      return res.status(400).json({ error: "Такой объект уже существует" });
+    }
+    if (isFolder) {
+      await fsPromises.mkdir(fullPath, { recursive: true });
+    } else {
+      await fsPromises.writeFile(fullPath, "", "utf-8");
+    }
+    res.json({ success: true });
+  } catch (err) {
+    console.error("Ошибка при создании файла", err);
+    res.status(500).json({ error: "Не удалось создать файл" });
   }
-  if (!fs.statSync(filePath).isFile()) {
-    return res.status(400).json({ error: "Path is not a file" });
+};
+export const deleteFilder = async (req: Request, res: Response) => {
+  try {
+    const { path: targetPath } = req.body;
+    if (!targetPath) {
+      return res.status(400).json({ error: "Отсутствует имя" });
+    }
+    if (!fs.existsSync(targetPath)) {
+      return res.status(400).json({ error: "Этого объекта не существует" });
+    }
+    const stats = fs.statSync(targetPath);
+    if (stats.isDirectory()) {
+      await fsPromises.rm(targetPath, { recursive: true, force: true });
+    } else {
+      await fsPromises.unlink(targetPath);
+    }
+    return res.json({ success: true });
+  } catch (err) {
+    console.error("Ошибка при удалении", err);
+    return res.status(500).json({ error: "Не удалось удалить объект" });
   }
-  await fsPromises.writeFile(filePath, content ?? "", "utf-8");
-  res.json({success:true});
-} catch (err) {
-  console.error("Ошибка при сохранении файла", err);
-  res.status(500).json({error: "Не удалось сохранить файл"})
-}
-}
+};
--- backend/src/routes/files.ts
+++ backend/src/routes/files.ts
@@ -1,7 +1,9 @@
-import { getFileContent, getElemsInFolder, saveFileChanges } from "../controllers/filesControler.ts";
+import { getFileContent, getElemsInFolder, saveFileChanges, createFilder, deleteFilder } from "../controllers/filesControler.ts";
 import express from "express";
 const router = express.Router()
 router.get('/',getElemsInFolder)
 router.get('/content', getFileContent)
 router.post('/save', saveFileChanges)
+router.post('/create', createFilder)
+router.post('/remove', deleteFilder)
 export default router;
